0cfb07d2cc5784a18889520cd4b17f49f5ede5d3
luoyunchong <luoyunchong@foxmail.com>
2020-12-01 01:19:23 +0800

    add send email demo 并集成console注入。

---
+++ Mail/VoVo.Email/VoVo.Email/App.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MimeKit;
+
+namespace VoVo.Email
+{
+    public class App
+    {
+        private readonly IEmailSender _emailSender;
+        private readonly ILogger<App> _logger;
+        public App(ILogger<App> logger, IEmailSender emailSender)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _emailSender = emailSender;
+        }
+        public async Task Run(string[] args)
+        {
+            _logger.LogInformation("Starting...");
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("igeekfan", "igeekfan@163.com"));
+            message.To.Add(new MailboxAddress("Mrs. luoyunchong", "luoyunchong@foxmail.com"));
+            message.Subject = "How you doin'?";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Hey Chandler,
+
+I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+-- Joey"
+
+            };
+            await _emailSender.SendAsync(message);
+
+            _logger.LogInformation("Finished!");
+            await Task.CompletedTask;
+        }
+    }
+}
